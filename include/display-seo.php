cb2e94b90d6c9724945d08fd15568b39c005502b
crawltrack <crawltrack@e9654ebe-cee9-4721-9195-11d088f91d6b>
2011-10-29 12:25:43 +0000

    suppression of Exalead, Yahoo, Bing & delicious from indexation (API depreciated)

--- include/display-seo.php
+++ include/display-seo.php
@@ -20,8 +20,6 @@ if (!defined('IN_CRAWLT')) {
 	exit('<h1>Hacking attempt !!!!</h1>');
 }
 //initialize array and variable
-$tablinkexalead = array();
-$tabpageexalead = array();
 $tablinkgoogle = array();
 $tabpagegoogle = array();
 if ($period >= 1000) {
@@ -62,21 +60,15 @@ $nbrresult = mysql_num_rows($requeteseo);
 if ($nbrresult >= 1) {
 	$i = 1;
 	while ($ligneseo = mysql_fetch_row($requeteseo)) {
-		$tablinkexalead[] = $ligneseo[5];
-		$tabpageexalead[] = $ligneseo[6];
 		$tablinkgoogle[] = $ligneseo[7];
 		$tabpagegoogle[] = $ligneseo[8];
 	}
 
 	//preparation of values for display
 	if ($period == 0 || $period >= 1000) {
-		$linkexalead = numbdisp($tablinkexalead[0]);
-		$pageexalead = numbdisp($tabpageexalead[0]);
 		$linkgoogle = numbdisp($tablinkgoogle[0]);
 		$pagegoogle = numbdisp($tabpagegoogle[0]);
 	} else {
-		$linkexalead = numbdisp($tablinkexalead[0]) . " --> " . numbdisp($tablinkexalead[($nbrresult - 1) ]);
-		$pageexalead = numbdisp($tabpageexalead[0]) . " --> " . numbdisp($tabpageexalead[($nbrresult - 1) ]);
 		$linkgoogle = numbdisp($tablinkgoogle[0]) . " --> " . numbdisp($tablinkgoogle[($nbrresult - 1) ]);
 		$pagegoogle = numbdisp($tabpagegoogle[0]) . " --> " . numbdisp($tabpagegoogle[($nbrresult - 1) ]);
 	}
@@ -118,22 +110,7 @@ if (($tabpagegoogle[0] == $tabpagegoogle[($nbrresult - 1) ]) && $tabpagegoogle[0
 } else {
 	echo "<td class='tableau5'>" . $pagegoogle . "</td></tr>\n";
 }
-echo "<tr><td class='tableau30g'>&nbsp;&nbsp;&nbsp;<a href=\"http://www.exalead.com\">" . $language['exalead'] . "</a>\n";
-if ($period == 0 && ($linkexalead == 0 || $pageexalead == 0)) {
-	echo "<a href=\"./php/searchenginespositionrefresh.php?retry=exalead&amp;navig=$navig&amp;period=$period&amp;site=$site&amp;crawler=$crawlencode&amp;graphpos=$graphpos\"><img src=\"./images/refresh.png\" width=\"16\" height=\"16\" border=\"0\" ></a></td>\n";
-} else {
-	echo "</td>\n";
-}
-if (($tablinkexalead[0] == $tablinkexalead[($nbrresult - 1) ]) && $tablinkexalead[0] == 0) {
-	echo "<td class='tableau30' >-</td>\n";
-} else {
-	echo "<td class='tableau30'>" . $linkexalead . "</td>\n";
-}
-if (($tabpageexalead[0] == $tabpageexalead[($nbrresult - 1) ]) && $tabpageexalead[0] == 0) {
-	echo "<td class='tableau50'>-</td></tr>\n";
-} else {
-	echo "<td class='tableau50'>" . $pageexalead . "</td></tr>\n";
-}
+
 echo "</table><br>\n";
 echo "</div><br>\n";
 if ($period != 5) {
