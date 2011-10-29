11282bc9b09b36697f854bd0f665f10390540339
crawltrack <crawltrack@e9654ebe-cee9-4721-9195-11d088f91d6b>
2011-10-29 09:44:05 +0000

    suppression of Yahoo & Bing from indexation (API depreciated)

--- include/searchenginesposition.php
+++ include/searchenginesposition.php
@@ -1,6 +1,6 @@
 <?php
 //----------------------------------------------------------------------
-//  CrawlTrack 3.2.8
+//  CrawlTrack 3.3.1
 //----------------------------------------------------------------------
 // Crawler Tracker for website
 //----------------------------------------------------------------------
@@ -14,7 +14,7 @@
 //----------------------------------------------------------------------
 // file: searchenginesposition.php
 //----------------------------------------------------------------------
-//  Last update: 16/02/2011
+//  Last update: 29/10/2011
 //----------------------------------------------------------------------
 //nusoap
 require_once (dirname(__FILE__).'/../nusoap/class.nusoap_base.php');
@@ -24,40 +24,7 @@ require_once (dirname(__FILE__).'/../nusoap/class.soap_parser.php');
 require_once (dirname(__FILE__).'/../nusoap/class.soap_fault.php');
 require_once (dirname(__FILE__).'/../nusoap/class.soapclient.php');
 
-//sortyahooXMLTree
-function sortyahooXMLTree($data) {
-	// create parser
-	$parser = xml_parser_create();
-	xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-	xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
-	xml_parse_into_struct($parser, $data, $values, $tags);
-	xml_parser_free($parser);
-	$result = $values[0]['attributes']['totalResultsAvailable'];
-	return $result;
-}
-function &composeArray($array, $elements, $value = array()) {
-	// get current element
-	$element = array_shift($elements);
-	// does the current element refer to a list
-	if ($element == "Result") {
-		// more elements?
-		if (sizeof($elements) > 0) {
-			@$array[$element][sizeof($array[$element]) - 1] = & composeArray(@$array[$element][sizeof(@$array[$element]) - 1], $elements, $value);
-		} else
-		// if (is_array($value))
-		{
-			@$array[$element][sizeof(@$array[$element]) ] = $value;
-		}
-	} else {
-		// more elements?
-		if (sizeof($elements) > 0) {
-			@$array[$element] = & composeArray(@$array[$element], $elements, $value);
-		} else {
-			@$array[$element] = $value;
-		}
-	}
-	return $array;
-}
+
 //-------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------
@@ -84,97 +51,7 @@ if ($crawltnbrresult >= 1) {
 //looking for position in search engines database using api
 //test loop position
 if ($crawltloop < $crawltnbrresult) {
-	//yahoo
-	$crawltidsite = $listsitecrawlt[$crawltloop];
-	$crawlturlsite = $crawltsiteurl[$crawltidsite];
-	if (empty($crawlturlsite)) {
-		//update the crawlt_config table
-		$crawltloopnext = $crawltloop + 1;
-		$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='0', loop1='" . crawlt_sql_quote($crawltloopnext) . "'";
-		$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
-	} else {
-		//to avoid problem if the url is enter in the database with http://
-		$crawlturlsite = strip_protocol($crawlturlsite);
-		if ($crawltloop2 == 0) {
-			$crawltrequete1 = "http://$crawlturlsite";
-			$crawltquery1 = "http://search.yahooapis.com/SiteExplorerService/V1/inlinkData?appid=crawltrack&omit_inlinks=domain&query=" . rawurlencode($crawltrequete1);
-			// Try to temporarly allow url_fopen
-			if (ini_get('allow_url_fopen') != 1) {
-				@ini_set('allow_url_fopen', '1');
-			}
-			$crawltnbryahoo1 = 0;
-			if (ini_get('allow_url_fopen') == 1) {
-				$crawltxml1 = file_get_contents($crawltquery1);
-				$crawltnbryahoo1 = sortyahooXMLTree($crawltxml1);
-				if (empty($crawltnbryahoo1)) {
-					$crawltnbryahoo1 = 0;
-				}
-			}
-			//check if the date already exists in the table
-			$crawltsqlcheck = "SELECT date,id_site, linkyahoo  FROM crawlt_seo_position
-                        WHERE date= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-                        AND id_site='" . crawlt_sql_quote($crawltidsite) . "'";
-			$crawltrequetecheck = mysql_query($crawltsqlcheck, $crawltconnexion);
-			$crawltnbrresultcheck = mysql_num_rows($crawltrequetecheck);
-			if ($crawltnbrresultcheck >= 1) {
-				$crawltligne = mysql_fetch_assoc($crawltrequetecheck);
-				$crawltexitingvalue = $crawltligne['linkyahoo'];
-				if($crawltexitingvalue == 0) {
-				$crawltsqlseo = "UPDATE crawlt_seo_position SET linkyahoo='" . crawlt_sql_quote($crawltnbryahoo1) . "'
-                          WHERE date= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-                          AND id_site='" . crawlt_sql_quote($crawltidsite) . "'";
-                      $crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
-					  }
-			} else {
-				$crawltsqlseo = "INSERT INTO crawlt_seo_position (date,id_site, linkyahoo, pageyahoo, linkmsn, pagemsn, nbrdelicious, tagdelicious, linkexalead, pageexalead, linkgoogle, pagegoogle) VALUES ( '" . crawlt_sql_quote($crawltdatetoday2) . "','" . crawlt_sql_quote($crawltidsite) . "','" . crawlt_sql_quote($crawltnbryahoo1) . "','0','0','0','0',' ','0','0','0','0')";
-				$crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
-			}
-			$crawltloop2next = $crawltloop2 + 1;
-			//update the crawlt_config table
-			$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='" . crawlt_sql_quote($crawltloop2next) . "'";
-			$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
-		} else {
-			$crawltrequete2 = "http://$crawlturlsite";
-			$crawltquery2 = "http://search.yahooapis.com/SiteExplorerService/V1/pageData?appid=crawltrack&query=" . rawurlencode($crawltrequete2);
-			// Try to temporarly allow url_fopen
-			if (ini_get('allow_url_fopen') != 1) {
-				@ini_set('allow_url_fopen', '1');
-			}
-			$crawltnbryahoo2 = 0;
-			if (ini_get('allow_url_fopen') == 1) {
-				$crawltxml2 = file_get_contents($crawltquery2);
-				$crawltnbryahoo2 = sortyahooXMLTree($crawltxml2);
-				if (empty($crawltnbryahoo2)) {
-					$crawltnbryahoo2 = 0;
-				}
-			}
-			//check if the date already exists in the table
-			$crawltsqlcheck = "SELECT date,id_site,pageyahoo  FROM crawlt_seo_position
-                        WHERE date= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-                        AND id_site='" . crawlt_sql_quote($crawltidsite) . "'";
-			$crawltrequetecheck = mysql_query($crawltsqlcheck, $crawltconnexion);
-			$crawltnbrresultcheck = mysql_num_rows($crawltrequetecheck);
-			if ($crawltnbrresultcheck >= 1) {
-				$crawltligne = mysql_fetch_assoc($crawltrequetecheck);
-				$crawltexitingvalue = $crawltligne['pageyahoo'];
-				if($crawltexitingvalue == 0) {
-				$crawltsqlseo = "UPDATE crawlt_seo_position SET pageyahoo='" . crawlt_sql_quote($crawltnbryahoo2) . "'
-                          WHERE date= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-                          AND id_site='" . crawlt_sql_quote($crawltidsite) . "'";
-                      $crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
-					  }
-			} else {
-				$crawltsqlseo = "INSERT INTO crawlt_seo_position (date,id_site, linkyahoo, pageyahoo, linkmsn, pagemsn, nbrdelicious, tagdelicious, linkexalead, pageexalead, linkgoogle, pagegoogle) VALUES ( '" . crawlt_sql_quote($crawltdatetoday2) . "','" . crawlt_sql_quote($crawltidsite) . "','0','" . crawlt_sql_quote($crawltnbryahoo2) . "','0','0','0',' ','0','0','0','0')";
-			$crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
-			}
-			//update the crawlt_config table
-			$crawltloopnext = $crawltloop + 1;
-			$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='0', loop1='" . crawlt_sql_quote($crawltloopnext) . "'";
-			$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
-		}
-	}
-} elseif ($crawltloop >= $crawltnbrresult && $crawltloop < $crawltnbrresult2) {
-	//msn
+	//Exalead
 	$crawltidsite = ($crawltloop - $crawltnbrresult);
 	$crawltsite = $listsitecrawlt[$crawltidsite];
 	$crawlturlsite = $crawltsiteurl[$crawltsite];
@@ -256,50 +133,13 @@ if ($crawltloop < $crawltnbrresult) {
 				$crawltsqlseo = "INSERT INTO crawlt_seo_position (date,id_site, linkyahoo, pageyahoo, linkmsn, pagemsn, nbrdelicious, tagdelicious, linkexalead, pageexalead, linkgoogle, pagegoogle) VALUES ( '" . crawlt_sql_quote($crawltdatetoday2) . "','" . crawlt_sql_quote($crawltsite) . "','0','0','0','0','0',' ','0','" . crawlt_sql_quote($crawltnbrexalead2) . "','0','0')";
 				$crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
 			}
-			$crawltloop2next = $crawltloop2 + 1;
-			//update the crawlt_config table
-			$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='" . crawlt_sql_quote($crawltloop2next) . "'";
-			$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
-		} else {
-			$crawltrequete2 = "site:$crawlturlsite";
-			$soapclient = new nusoap_client("http://soap.search.msn.com/webservices.asmx");
-			$crawltparam2 = array('AppID' => '5E4A1FC1F7B268DD7BCE62F39BFF8A0D81CB900B', 'Query' => $crawltrequete2, 'CultureInfo' => 'en-US', 'SafeSearch' => 'Off', 'Requests' => array('SourceRequest' => array('Source' => 'Web', 'Offset' => 0, 'Count' => 50, 'ResultFields' => 'All'),),);
-			$crawltsearchresults2 = $soapclient->call("Search", array("Request" => $crawltparam2));
-			if (!empty($crawltsearchresults2)) {
-				$crawltnbrmsn2 = $crawltsearchresults2['Responses']['SourceResponse']['Total'];
-			} else {
-				$crawltnbrmsn2 = 0;
-			}
-			if (empty($crawltnbrmsn2)) {
-				$crawltnbrmsn2 = 0;
-			}
-			//insert values in the crawlt_seo_position table
-			//check if the date already exists in the table
-			$crawltsqlcheck = "SELECT `date`, id_site, pagemsn  FROM crawlt_seo_position
-				WHERE `date`= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-				AND id_site='" . crawlt_sql_quote($crawltsite) . "'";
-			$crawltrequetecheck = mysql_query($crawltsqlcheck, $crawltconnexion);
-			$crawltnbrresultcheck = mysql_num_rows($crawltrequetecheck);
-			if ($crawltnbrresultcheck >= 1) {
-				$crawltligne = mysql_fetch_assoc($crawltrequetecheck);
-				$crawltexitingvalue = $crawltligne['pagemsn'];
-				if($crawltexitingvalue == 0) {
-				$crawltsqlseo = "UPDATE crawlt_seo_position SET pagemsn='" . crawlt_sql_quote($crawltnbrmsn2) . "'
-					WHERE `date`= '" . crawlt_sql_quote($crawltdatetoday2) . "'
-					AND id_site='" . crawlt_sql_quote($crawltsite) . "'";
-				$crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);
-				}
-			} else {
-				$crawltsqlseo = "INSERT INTO crawlt_seo_position (`date`, id_site, linkyahoo, pageyahoo, linkmsn, pagemsn, nbrdelicious, tagdelicious, linkexalead, pageexalead, linkgoogle, pagegoogle) VALUES ( '" . crawlt_sql_quote($crawltdatetoday2) . "','" . crawlt_sql_quote($crawltsite) . "','0','0','0','" . crawlt_sql_quote($crawltnbrmsn2) . "','0',' ','0','0','0','0')";
-				$crawltrequeteseo = mysql_query($crawltsqlseo, $crawltconnexion);			
-			}
 			//update the crawlt_config table
 			$crawltloopnext = $crawltloop + 1;
 			$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='0', loop1='" . crawlt_sql_quote($crawltloopnext) . "'";
 			$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
-		}
+		} 
 	}
-} elseif ($crawltloop >= $crawltnbrresult2 && $crawltloop < $crawltnbrresult3) {
+} elseif ($crawltloop >= $crawltnbrresult2 && $crawltloop < $crawltnbrresult2) {
 	//del.icio.us
 	//initialize array & variables
 	$crawltnbrpertag = array();
@@ -379,7 +219,7 @@ if ($crawltloop < $crawltnbrresult) {
 		$crawltsqlupdateseo = "UPDATE crawlt_config SET loop2='0', loop1='" . crawlt_sql_quote($crawltloopnext) . "'";
 		$crawltrequeteupdateseo = mysql_query($crawltsqlupdateseo, $crawltconnexion);
 	}
-} elseif ($crawltloop >= $crawltnbrresult3 && $crawltloop < $crawltnbrresult4) {
+} elseif ($crawltloop >= $crawltnbrresult3 && $crawltloop < $crawltnbrresult3) {
 	//google
 	$crawltidsite = ($crawltloop - $crawltnbrresult3);
 	$crawltsite = $listsitecrawlt[$crawltidsite];
