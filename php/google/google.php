836aa910b98ad2d2b8ce18c9b2f2b8f4759c311a
crawltrack <crawltrack@e9654ebe-cee9-4721-9195-11d088f91d6b>
2011-11-11 22:13:17 +0000

    change to curl and also echo error message from Google in case of issue

--- php/google/google.php
+++ php/google/google.php
@@ -1,6 +1,6 @@
 <?php
 //----------------------------------------------------------------------
-//  CrawlTrack 3.3.1
+//  CrawlTrack 3.3.2
 //----------------------------------------------------------------------
 // Crawler Tracker for website
 //----------------------------------------------------------------------
@@ -14,69 +14,85 @@
 //----------------------------------------------------------------------
 // file: google.php
 //----------------------------------------------------------------------
-//  Last update: 29/10/2011
+//  Last update: 11/11/2011
 //----------------------------------------------------------------------
 error_reporting(0);
+$times=0;
+$period=0;
+$formattedresults='';
 include ("../../include/functions.php");
 //call back the page
 $keywordurl=$_GET['q'];
+$crawltcharset=1;
 $keyworddisplay = stripslashes(crawltcuturl($keywordurl, '55'));
 $crawltlang=$_GET['lang'];
+
 require_once('JSON.phps');
 
-$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" . rawurlencode($keywordurl);	
-$handle = fopen($url, 'rb');
-$body = '';
-while (!feof($handle)) {
-$body .= fread($handle, 8192);
-}
-fclose($handle);
+$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" . rawurlencode($keywordurl);
 
+$referer=$_SERVER['HTTP_HOST'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_REFERER, $referer);
+$body = curl_exec($ch);
+curl_close($ch);
 $json = new Services_JSON();
 $json = $json->decode($body);
- 
-foreach($json->responseData->results as $searchresult)
-	{
-	if($searchresult->GsearchResultClass == 'GwebSearch')
-		{
-		$formattedresults .= '
-		<div class="google">
-		<h3><a href="' . $searchresult->unescapedUrl . '">' . $searchresult->titleNoFormatting . '</a></h3>
-		<p class="content">' . $searchresult->content . '</p>
-		<p class="linkurl">' . $searchresult->visibleUrl . '</p>
-		</div>';
-		}
-	}
-sleep(3);	
-$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=5&q=" . rawurlencode($keywordurl);	
-$handle = fopen($url, 'rb');
-$body = '';
-while (!feof($handle)) {
-$body .= fread($handle, 8192);
-}
-fclose($handle);
 
-$json = new Services_JSON();
-$json = $json->decode($body);
- 
-foreach($json->responseData->results as $searchresult)
-	{
-	if($searchresult->GsearchResultClass == 'GwebSearch')
+
+ if(is_array($json->responseData->results))
+ 	{
+	foreach($json->responseData->results as $searchresult)
 		{
-		$formattedresults .= '
-		<div class="google">
-		<h3><a href="' . $searchresult->unescapedUrl . '">' . $searchresult->titleNoFormatting . '</a></h3>
-		<p class="content">' . $searchresult->content . '</p>
-		<p class="linkurl">' . $searchresult->visibleUrl . '</p>
-		</div>';
+		if($searchresult->GsearchResultClass == 'GwebSearch')
+			{
+			$formattedresults .= '
+			<div class="google">
+			<h3><a href="' . $searchresult->unescapedUrl . '">' . $searchresult->titleNoFormatting . '</a></h3>
+			<p class="content">' . $searchresult->content . '</p>
+			<p class="linkurl">' . $searchresult->visibleUrl . '</p>
+			</div>';
+			}
+		}
+		
+	$url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=5&q=" . rawurlencode($keywordurl);	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_REFERER, $referer);
+	$body = curl_exec($ch);
+	curl_close($ch);
+	$json = new Services_JSON();
+	$json = $json->decode($body);
+	 
+	foreach($json->responseData->results as $searchresult)
+		{
+		if($searchresult->GsearchResultClass == 'GwebSearch')
+			{
+			$formattedresults .= '
+			<div class="google">
+			<h3><a href="' . $searchresult->unescapedUrl . '">' . $searchresult->titleNoFormatting . '</a></h3>
+			<p class="content">' . $searchresult->content . '</p>
+			<p class="linkurl">' . $searchresult->visibleUrl . '</p>
+			</div>';
+			}
 		}
 	}
+else
+	{
+	$formattedresults=$body;
+	}
+
 	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link rel="stylesheet" type="text/css" href="stylegoogle.css" media="screen" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <?php
