176db4870bbd5324b55cc7d62d8aa0e40fe4ee00
crawltrack <crawltrack@e9654ebe-cee9-4721-9195-11d088f91d6b>
2011-11-18 22:38:14 +0000

    Update Ask host list

--- include/searchenginelist.php
+++ include/searchenginelist.php
@@ -21,7 +21,7 @@
  * Authors : phpMyVisites team and community
 */
 //----------------------------------------------------------------------
-//  Last update: 16/11/2011
+//  Last update: 18/11/2011
 //----------------------------------------------------------------------
 $crawltcssaattack = array("http:", "ftp:", "https:", "http%3a", "https%3a", "ht%20tp:", "htt%20p:", "http%20:", "h%20ttp:", "/etc/passwd", "ShellAdresi.TXT", "[evil_root]?", "http%3A%2F%2F","%68%74%74%70%3a","%68%74%74%70%73%3a","%48%54%54%50%3a","%48%54%54%50%53%3a","%3C?");
 $crawltsqlaattack = array("%20select%20", "%20like%20", "%20or%20", "%20where%20","%20SELECT%20", "%20LIKE%20", "%20OR%20", "%20WHERE%20");
@@ -739,6 +739,7 @@ $crawltasklist = array(
 "mx.ask.com",
 "nl.ask.com",
 "pe.ask.com",
+"pl.ask.com",
 "sg.ask.com",
 "tr.ask.com",
 "uk.ask.com",
