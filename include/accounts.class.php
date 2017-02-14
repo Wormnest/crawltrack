97d7d94ad3be555aa5d23f1cacdf790d1e747676
Jacob Boerema <jgboerema@gmail.com>
2017-02-14 12:09:48 +0100

    Add: database and account handling classes. Account handling will transparently upgrade password storage to safe password hashes. Upgrading will happen the first time a user logs in AFTER an upgrade has finished. So after upgrading an admin will have to log out and log in again before the change takes effect.

---
+++ include/accounts.class.php
@@ -0,0 +1,307 @@
+<?php
+/**
+ *  CrawlTrack class for handling user accounts.
+ *  CrawlTrack. A crawler tracker for websites.
+ *  License: GPL version 2 or newer. See license.txt.
+ *  File: Password.class.php
+ *  Author: Jacob Boerema
+ */
+
+if (!defined('IN_CRAWLT')) {
+	exit('<h1>No direct access</h1>');
+}
+
+/**
+ * TODO
+ * - Add maximum login tries within a certain time
+ * - Add minimum length of password
+ * - Maybe check for certain very common entered passwords
+ * - Require maybe numbers, non alphanumeric characters, minimum length of pw
+ */
+class ctAccounts
+{
+	private $db;
+	
+	public $rightsite;
+	public $rightadmin;
+	
+	/**
+	 * Init ctPassword class with a valid ctDb instance.
+	 */
+	public function __construct($db) {
+		$this->db = $db;
+	}
+	
+	/**
+	 * Checks if a username already exists. Returns true if it does otherwise false.
+	 */
+	public function username_exists($user) {
+		$sql = "SELECT * FROM crawlt_login WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('s', $user);
+		$result = $stmt->execute();
+		$stmt->store_result();
+		if ($result && $stmt->num_rows > 0) {
+			$result = true;
+		} else {
+			$result = false;
+		}
+		$stmt->close();
+		return $result;
+	}
+	
+	/**
+	 * Add an admin user account.
+	 */
+	public function add_admin($username, $password) {
+		// Admin accounts can only be added when installing or by an admin
+		if (!(defined('IN_CRAWLT_INSTALL') || defined('IN_CRAWLT_ADMIN'))) {
+			exit('<h1>Function does not exist</h1>');
+		}
+		return $this->add_user_account($username, $password, 0, 1);
+	}
+
+	/**
+	 * Add an non admin user account specifying which site(s) can be viewed.
+	 * Where $site==0 means all sites.
+	 */
+	public function add_nonadmin_user($username, $password, $site) {
+		return $this->add_user_account($username, $password, $site, 0);
+	}
+	
+	/**
+	 * Add a user account.
+	 */
+	private function add_user_account($username, $password, $site, $admin) {
+		$sql = "INSERT INTO crawlt_login (crawlt_user,admin,site,password_type,password_hash) VALUES (?,?,?,?,?)";
+		$pw_type = 1; // 1 = new/safe hashing
+		$pw_hash = $this->encode_password($password);
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('siiis', $username, $admin, $site, $pw_type, $pw_hash);
+		$result = $stmt->execute();
+		if ($result) {
+			$result = $stmt->affected_rows > 0;
+		}
+		$stmt->close();
+		return $result;
+	}
+	
+	/**
+	 * Check if the credentials are from a valid user.
+	 * Version using bind_param and bind_result
+	 */
+	public function is_valid_login($loginuser, $loginpassword) {
+		//$encoded_pw_old = $this->encode_password_old($loginpassword);
+		$sql = "SELECT * FROM crawlt_login WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('s', $loginuser);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($dummyid, $user, $password, $admin, $site, $type, $hash);
+		// TODO: Only one row is probably allowed since user should be unique?
+		// However need to check with non admin users and the site code!
+		// $site = 0 means is allowed to visit all sites, other number: only allowed to visit that site id
+		$result = false;
+		while ($row = $stmt->fetch()) {
+			if ($user == $loginuser) {
+				if ($type == 1) {
+					// Using new safe password hash
+					if (password_verify($loginpassword, $hash)) {
+						$result = true;
+					}
+				} elseif ($type == 0) {
+					// Using old md5 hashed password
+					if ($password == $this->encode_password_old($loginpassword)) {
+						// Password is correct. Now we need to update it
+						if ($this->update_password_encoding($user, $loginpassword)) {
+							$result = true;
+						}
+					}
+				}
+				
+				if ($result) {
+					$this->rightsite = $site;
+					$this->rightadmin = $admin;
+					$stmt->close();
+					return true;
+				}
+			}
+		}
+		$stmt->close();
+		return false;
+	}
+	
+	/**
+	 * Check if the credentials are from a valid user.
+	 * Old version needing quoting
+	 * TESTING VERSION USING OLD PASSWORD HANDLING. DONT USE!!
+	 */
+	public function is_valid_login0($loginuser, $loginpassword) {
+		$sql = "SELECT * FROM crawlt_login WHERE crawlt_user='" . $this->db->sql_quote($loginuser) . "'";
+		$result = $this->db->connexion->query($sql) or die("MySQL query error");
+		$encoded_pw_old = $this->encode_password_old($loginpassword);
+		// TODO: Only one row is probably allowed since user should be unique?
+		// However need to check with non admin users and the site code!
+		// $site = 0 means is allowed to visit all sites, other number: only allowed to visit that site id
+		while ($row = $result->fetch_object()) {
+			$user = $row->crawlt_user;
+			$password = $row->crawlt_password;
+			if ($user == $loginuser && $password == $encoded_pw_old) {
+				$this->rightsite = $row->site;
+				$this->rightadmin = $row->admin;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Check if the credentials are from a valid user.
+	 * Version using bind_param and automatic fetching of all results using fetch_results
+	 * TESTING VERSION USING OLD PASSWORD HANDLING. DONT USE!!
+	 */
+	public function is_valid_login2($loginuser, $loginpassword) {
+		$encoded_pw_old = $this->encode_password_old($loginpassword);
+		$sql = "SELECT * FROM crawlt_login WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('s', $loginuser);
+		$stmt->execute();
+		$results = ctDb::fetch_results($stmt);
+		// TODO: Only one row is probably allowed since user should be unique?
+		// However need to check with non admin users and the site code!
+		// $site = 0 means is allowed to visit all sites, other number: only allowed to visit that site id
+		foreach ($results as $res) {
+			$password = $res['crawlt_password'];
+			$user = $res['crawlt_user'];
+			if ($user == $loginuser && $password == $encoded_pw_old) {
+				$this->rightsite = $res['crawlt_site'];
+				$this->rightadmin = $res['crawlt_admin'];
+				$stmt->close();
+				return true;
+			}
+		}
+		$stmt->close();
+		return false;
+	}
+	
+	/**
+	 * Check if the credentials are from a valid user.
+	 * Version using bind_param and automatic fetching of all results using fetch_assoc_stmt
+	 * TESTING VERSION USING OLD PASSWORD HANDLING. DONT USE!!
+	 */
+	public function is_valid_login3($loginuser, $loginpassword) {
+		$encoded_pw_old = $this->encode_password_old($loginpassword);
+		$sql = "SELECT * FROM crawlt_login WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('s', $loginuser);
+		$stmt->execute();
+		$results = ctDb::fetch_assoc_stmt($stmt);
+		// TODO: Only one row is probably allowed since user should be unique?
+		// However need to check with non admin users and the site code!
+		// $site = 0 means is allowed to visit all sites, other number: only allowed to visit that site id
+		foreach ($results as $res) {
+			$password = $res['crawlt_password'];
+			$user = $res['crawlt_user'];
+			if ($user == $loginuser && $password == $encoded_pw_old) {
+				$this->rightsite = $res['crawlt_site'];
+				$this->rightadmin = $res['crawlt_admin'];
+				$stmt->close();
+				return true;
+			}
+		}
+		$stmt->close();
+		return false;
+	}
+	
+	/**
+	 * Get a list of all non admin usernames.
+	 */
+	public function get_all_nonadmin_users() {
+		$users = array();
+		$sql = "SELECT crawlt_user FROM crawlt_login WHERE admin=0";
+		$stmt = $this->db->connexion->prepare($sql);
+		$result = $stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($user);
+		while ($row = $stmt->fetch()) {
+			$users[] = $user;
+		}
+		$stmt->close();
+		return $users;
+	}
+	
+	/**
+	 * Delete a user.
+	 * TODO: Should we add an extra check here to make sure it's not an admin account?
+	 */
+	public function delete_user_account($user) {
+		// Only Admins can delete an account
+		if (!defined('IN_CRAWLT_ADMIN')) {
+			exit('<h1>Function does not exist</h1>');
+		}
+		$sql = "DELETE FROM crawlt_login WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('s', $user);
+		$result = $stmt->execute();
+		if ($result) {
+			$result = $stmt->affected_rows > 0;
+		}
+		$stmt->close();
+		return $result;
+	}
+	
+	/**
+	 * Change password.
+	 */
+	public function change_password($user, $newpassword) {
+		// At this point account will always be already type 1 (using new password hash)
+		// because before changing password we will always check current password with user first
+		$encoded_pw = $this->encode_password($newpassword);
+		$sql = "UPDATE crawlt_login SET password_hash=? WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$stmt->bind_param('ss', $encoded_pw, $user);
+		$result = $stmt->execute();
+		if ($result) {
+			$result = $stmt->affected_rows > 0;
+		}
+		$stmt->close();
+		return $result;
+	}
+	
+	/**
+	 * Compatibility function to update an old deprecated password encoding to using a new hash.
+	 */
+	private function update_password_encoding($user, $password) {
+		$sql = "UPDATE crawlt_login SET crawlt_password=NULL, password_type='1', password_hash=? WHERE crawlt_user=?";
+		$stmt = $this->db->connexion->prepare($sql);
+		$hash = $this->encode_password($password);
+		$stmt->bind_param('ss', $hash, $user);
+		$result = $stmt->execute();
+		if ($result) {
+			$result = $stmt->affected_rows > 0;
+		}
+		$stmt->close();
+		return $result;
+	}
+	
+	/**
+	 * Encode a password with the recommended password hashing function.
+	 */
+	private function encode_password($pw) {
+		// password functions built in starting php 5.5, before that use compat library
+		if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+			$compatfile = dirname(__FILE__) . '/../vendor/ircmaxell/password-compat/lib/password.php';
+			require_once($compatfile);
+		}
+		return password_hash($pw, PASSWORD_DEFAULT);
+	}
+	
+	/**
+	 * Old deprecated password encoding. Don't use!
+	 */
+	private function encode_password_old($pw) {
+		return md5($pw);
+	}
+}
+
+?>
---
+++ include/db.class.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ *  CrawlTrack class for database access.
+ *  CrawlTrack. A crawler tracker for websites.
+ *  License: GPL version 2 or newer. See license.txt.
+ *  File: Password.class.php
+ *  Author: Jacob Boerema
+ */
+
+if (!defined('IN_CRAWLT')) {
+	exit('<h1>No direct access</h1>');
+}
+
+/**
+ * Class for handling database access.
+ */
+class ctDb
+{
+	public $connexion;
+	
+	public $secret_key;
+	
+	// $oldversion is true if the version we are updating from is < 150
+	public $oldversion = false;
+	
+	private $configfile;
+	
+	/**
+	 * Init our class and connect to the database.
+	 */
+	public function __construct() {
+		$this->configfile = dirname(__FILE__) . '/configconnect.php';
+		if (!file_exists($this->configfile)) {
+			$this->configfile = '';
+			die("config file not found!");
+		} else {
+			// Open database configuration settings
+			// TODO For now we use require but we should see if we could change this
+			// so we can use require_once or that we have a $config parameter in the
+			// constructor that has a config class object with all these details!!!
+			// TODO2: Counting of queries. Posisbly handle via a class var in config class.
+			require($this->configfile);
+			$this->secret_key = $secret_key;
+			if (!isset($crawlthost)) {
+				// Older configuration files before version 150 had different names
+				$this->oldversion = true;
+				$crawlthost = $host;
+				$crawltuser = $user;
+				$crawltpassword = $password;
+				$crawltdb = $db;
+			}
+			$this->connexion = mysqli_connect($crawlthost, $crawltuser, $crawltpassword, $crawltdb) or die("MySQL: problem connecting to database");
+			if (mysqli_connect_errno()) {
+				die(mysqli_connect_error());
+			}
+		}
+	}
+	
+	/**
+	 * Close the database connection.
+	 */
+	public function close() {
+		 if ($this->connexion) {
+			mysqli_close($this->connexion);
+		 }
+	}
+	
+	/**
+	 * Escape a query string.
+	 */
+	public function sql_quote($value) {
+		if (get_magic_quotes_gpc()) {
+			$value = stripslashes($value);
+		}
+		$value = $this->connexion->real_escape_string($value);
+		return $value;
+	}
+	
+	// More or less the same as the next function fetch_assoc_stmt
+	// Since that last one uses copy_value instead of a loop I assume that
+	// one might be faster but should be tested sometime.
+	// See: http://php.net/manual/en/mysqli-stmt.bind-result.php#102179
+	public static function fetch_results($result)
+	{   
+		$array = array();
+	   
+		if($result instanceof mysqli_stmt)
+		{
+			$result->store_result();
+		   
+			$variables = array();
+			$data = array();
+			$meta = $result->result_metadata();
+		   
+			while($field = $meta->fetch_field())
+				$variables[] = &$data[$field->name]; // pass by reference
+		   
+			call_user_func_array(array($result, 'bind_result'), $variables);
+		   
+			$i=0;
+			while($result->fetch())
+			{
+				$array[$i] = array();
+				foreach($data as $k=>$v)
+					$array[$i][$k] = $v;
+				$i++;
+			   
+				// don't know why, but when I tried $array[] = $data, I got the same one result in all rows
+			}
+		}
+		elseif($result instanceof mysqli_result)
+		{
+			while($row = $result->fetch_assoc())
+				$array[] = $row;
+		}
+	   
+		return $array;
+	}
+
+	/**
+	* Fetches the results of a prepared statement as an array of associative
+	* arrays such that each stored array is keyed by the result's column names.
+	* @param stmt   Must have been successfully prepared and executed prior to calling this function
+	* @param buffer Whether to buffer the result set; if true, results are freed at end of function
+	* @return An array, possibly empty, containing one associative array per result row
+	* See: http://php.net/manual/en/mysqli-stmt.fetch.php#117275
+	*/
+	public static function fetch_assoc_stmt(\mysqli_stmt $stmt, $buffer = true) {
+		if ($buffer) {
+			$stmt->store_result();
+		}
+		$fields = $stmt->result_metadata()->fetch_fields();
+		$args = array();
+		foreach($fields AS $field) {
+			$key = str_replace(' ', '_', $field->name); // space may be valid SQL, but not PHP
+			$args[$key] = &$field->name; // this way the array key is also preserved
+		}
+		call_user_func_array(array($stmt, "bind_result"), $args);
+		$results = array();
+		while($stmt->fetch()) {
+			$results[] = array_map("copy_value", $args);
+		}
+		if ($buffer) {
+			$stmt->free_result();
+		}
+		return $results;
+	}
+}
+
+/**
+* Copy value as value
+* JB: Doesn't seem to work when put inside a class
+*/
+function copy_value($v) {
+	return $v;
+}
+
+?>
